3525392b9a2da45b137fe6a36ec69d44e608d6b5
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-08-04 00:32:37 +0100

    commit

--- pset9.py
+++ pset9.py
@@ -42,7 +42,7 @@ df_dummy = pd.get_dummies(df, columns=['weekday', 'president'])
 # Print the new dataframe with the dummy variables.
 print("Dataframe after converting 'Weekday' and 'President' to dummy variables:")
 print(df_dummy.head())
-print(df_dummy)
+
 # Step 3: Conduct a linear regression to investigate the relationship between 'sscore', 'sp500', and 'ibm'.
 result = ols(formula='ibm ~ sscore + sp500', data=df_dummy).fit()
 print(result.summary())
@@ -51,6 +51,32 @@ print(result.summary())
 # for the intercept and the predictors ('sscore' and 'Sp500'). The p-value for each predictor tests the null hypothesis that 
 # the predictor's coefficient is zero, assuming that all other predictors are in the model. If the p-value is less than 0.05,
 # we can reject this null hypothesis and conclude that the predictor has a significant effect on the response ('ibm').
+
+# Store the adjusted R-squared in a variable named adj_rsquared
+adj_rsquared = result.rsquared_adj
+print("Adjusted R-squared:", adj_rsquared)
+
+# Store the p-value of F-statistics in a variable named f_pvalue
+f_pvalue = result.f_pvalue
+print("p-value of F-statistics:", f_pvalue)
+
+# Store the p-value of sscore in a variable named sscore_pvalue
+sscore_pvalue = result.pvalues['sscore']
+print("p-value of sscore:", sscore_pvalue)
+
+# Store the p-value of sp500 in a variable named sp500_pvalue
+sp500_pvalue = result.pvalues['sp500']
+print("p-value of sp500:", sp500_pvalue)
+
+# If a relationship exists between sscore and ibm stock price, then store a boolean value of
+# True in a variable named sscore_rel; otherwise, sscore_rel should be set to False
+sscore_rel = sscore_pvalue < 0.05  # We use 0.05 as the threshold for statistical significance
+print("Is sscore related to IBM stock price?", sscore_rel)
+
+# If a relationship exists between s&p 500 index and ibm stock price, then store a boolean value of True in a variable named sp500_rel; 
+# otherwise, sp500_rel should be set to False
+sp500_rel = sp500_pvalue < 0.05  # We use 0.05 as the threshold for statistical significance
+print("Is S&P 500 index related to IBM stock price?", sp500_rel)
 
 # Step 4: Predict whether the 'President' will be Republican or Democrat based on 'Sp500', 'Ibm', and 'sscore' using logistic regression.
 X = df[['sp500', 'ibm', 'sscore']]
